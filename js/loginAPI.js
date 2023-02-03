a3c801d79dc370d147a9341e0421677b7320286a
mengteck-liew <s10247866@connect.np.edu.sg>
2023-02-03 20:01:25 +0800

    removed loginvalidate.js

---
+++ js/loginAPI.js
@@ -0,0 +1,27 @@
+//[STEP 0]: Make sure our document is A-OK
+$(document).ready(function () {
+    //what kind of interface we want at the start 
+    const APIKEY = "63b64979969f06502871aa45";
+    $(".animation").hide();
+    //[STEP 1]: Create our submit form listener
+    $("#submit").on("click", function (e) {
+      //prevent default action of the button 
+      e.preventDefault();
+     
+        //[STEP 2]: let's retrieve form data
+        //for now we assume all information is valid
+        //you are to do your own data validation
+        let username = $("#username").val();
+        let email = $("#email").val();
+        let password = $("#password1").val();
+    
+        //[STEP 3]: get form values when user clicks on send
+        //Adapted from restdb api
+        let jsondata = {
+          "username": username,
+          "email": email,
+          "password" : password
+        };
+    });
+    
+});
